5b1bc1360296a859aa9f5834234b3e915714e4e3
Ivan Brygar <smitbmx@gmail.com>
2020-11-28 19:05:36 +0800

    Added a solution for Easy algorithms and an example with LogAllPairsOfArray task.

---
+++ Easy/EasyAlgorithms/LogAllPairsOfArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LogAllPairsOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] {1,2,3,4,5 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] != numbers[j])
+                    {
+                        Console.WriteLine(numbers[i] + " " + numbers[j]);
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

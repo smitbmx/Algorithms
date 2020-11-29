b8d55644f1e17e37e6c7a25550840b468d1e78ec
Ivan Brygar <smitbmx@gmail.com>
2020-11-29 09:20:41 +0800

    Added an easy algorithm of the array sum.

--- Easy/EasyAlgorithms/LogAllPairsOfArray/Program.cs
+++ Easy/EasyAlgorithms/LogAllPairsOfArray/Program.cs
@@ -19,7 +19,7 @@ namespace LogAllPairsOfArray
                 }
             }
 
-            Console.ReadLine();
+            Console.ReadLine(); 
         }
     }
 }
---
+++ Easy/EasyAlgorithms/SimpleArraySum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ar = new int[] { 4,3,2,4,5,7,8,9,3 };
+            int result = simpleArraySum(ar);
+
+            Console.WriteLine(result);
+        }
+
+        static int simpleArraySum(int[] ar)
+        {
+            int sum = 0;
+            foreach (var item in ar)
+            {
+                sum += item;
+            }
+
+            return sum;
+        }
+    }
+}

9bbf8746c1b3add45084b64c986eb85713ef444b
Ivan Brygar <smitbmx@gmail.com>
2020-11-29 16:32:45 +0800

    Reverse array task.

---
+++ Easy/EasyAlgorithms/ReverseArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int[] res = reverseArray(arr);
+
+            Console.WriteLine(string.Join(" ", res));
+        }
+
+        static int[] reverseArray(int[] a)
+        {
+            int[] reversedArray = new int[a.Length];
+            int lastIndex = a.Length-1;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                reversedArray[i] = a[lastIndex];
+                lastIndex--;
+            }
+
+            return reversedArray;
+        }
+    }
+}

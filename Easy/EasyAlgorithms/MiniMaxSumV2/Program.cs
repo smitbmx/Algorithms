de92e6f26af7ca7cc82ebd195473175d748a8e8d
Ivan Brygar <smitbmx@gmail.com>
2020-11-29 16:05:25 +0800

    Added another solution for the MiniMaxSum;

---
+++ Easy/EasyAlgorithms/MiniMaxSumV2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace MiniMaxSumV2
+{
+    class Program
+    {
+        static void miniMaxSum(int[] arr)
+        { 
+            long maxValue = arr.Max();
+            long minValue = arr.Min();
+            long sumValue = arr.Sum();
+
+            long miniSum = sumValue - maxValue;
+            long maxSum = sumValue - minValue;
+
+            Console.WriteLine(miniSum + " " + maxSum);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+
+            Console.WriteLine();
+        }
+    }
+}

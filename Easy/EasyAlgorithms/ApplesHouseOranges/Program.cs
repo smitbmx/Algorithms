7292561829ce2839ad0a9b7b098536211a268fd1
Ivan Brygar <smitbmx@gmail.com>
2020-11-30 14:13:13 +0800

    Solved Apple and Oranges task.

--- Easy/EasyAlgorithms/ApplesHouseOranges/Program.cs
+++ Easy/EasyAlgorithms/ApplesHouseOranges/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 
+/// <summary>
+/// https://www.hackerrank.com/challenges/apple-and-orange/problem
+/// </summary>
 namespace ApplesHouseOranges
 {
     class Program
     {
         static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
         {
+            int landedApples = 0;
+            int landedOranges = 0;
 
+            foreach (var apple in apples)
+            {
+                //House range
+                if (a + apple >= s && a + apple <= t)
+                {
+                    landedApples += 1;
+                }
+            }
 
+            foreach (var orange in oranges)
+            {
+                //House range
+                if (b + orange <= t && b + orange >= s)
+                {
+                    landedOranges += 1;
+                }
+            }
+
+            Console.WriteLine(landedApples);
+            Console.WriteLine(landedOranges);
         }
 
         static void Main(string[] args)

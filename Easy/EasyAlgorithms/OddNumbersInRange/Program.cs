0cc5d4fb771b16ed85c4b45b4611a11797cef186
Ivan Brygar <smitbmx@gmail.com>
2020-12-01 11:02:02 +0800

    Added a task about OddNumbersInRange

---
+++ Easy/EasyAlgorithms/OddNumbersInRange/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddNumbersInRange
+{
+    class Program
+    {
+        public static List<int> oddNumbers(int l, int r)
+        {
+            List<int> oddNumbersInRange = new List<int>();
+
+            for (int i = l; i <= r; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddNumbersInRange.Add(i);
+                }
+            }
+
+            return oddNumbersInRange;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var oddNumbersResult = oddNumbers(3, 9);
+
+            foreach (var number in oddNumbersResult)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

1acae41dcc81bb64122c7f3dda26dffd567695c2
Ivan Brygar <smitbmx@gmail.com>
2020-12-01 11:02:16 +0800

    Added zTest stub.

---
+++ Easy/EasyAlgorithms/zTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

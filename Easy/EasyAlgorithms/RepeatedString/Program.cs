35e6c62bece28d191d3f8c2b2f74b99f7a0da7bc
Ivan Brygar <smitbmx@gmail.com>
2020-12-02 14:57:01 +0800

    Added algo for RepeatedString task.

---
+++ Easy/EasyAlgorithms/RepeatedString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace RepeatedString
+{
+    class Program
+    {
+        static long repeatedString(string s, long n)
+        {
+            char charToSearch = 'a';
+            long amountOfFullSubstrings = n / s.Length;
+            long remainder = n - amountOfFullSubstrings * s.Length;
+            string remainderStr = s.Substring(0, (int)remainder);
+
+            long countOfCharInString = s.Count(c => c == charToSearch);
+            long result = countOfCharInString * amountOfFullSubstrings + remainderStr.Count(c => c == charToSearch);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            long n = Convert.ToInt64(Console.ReadLine());
+
+            long result = repeatedString(s, n);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}

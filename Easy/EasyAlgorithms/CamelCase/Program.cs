46210f780bb14233131309525b4ce98965db4b31
Ivan Brygar <smitbmx@gmail.com>
2020-12-03 16:14:48 +0800

    Solved CamelCase task.

---
+++ Easy/EasyAlgorithms/CamelCase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static int camelcase(string s)
+        {
+            int wordAmount = 0;
+            int theFirstWord = 1;
+
+            foreach (Char c in s)
+            {
+                if (Char.IsUpper(c))
+                {
+                    wordAmount++;
+                }
+            }
+
+            return wordAmount + theFirstWord;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int result = camelcase(s);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}

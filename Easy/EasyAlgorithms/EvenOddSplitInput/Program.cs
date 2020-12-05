72c2bdde8fcf5b32cbad03455a14d1a5cdd3490b
Ivan Brygar <smitbmx@gmail.com>
2020-12-05 19:02:08 +0800

    Added EvenOdd split input task.

---
+++ Easy/EasyAlgorithms/EvenOddSplitInput/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenOddSplitInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> arr = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd());
+            }
+
+            foreach (var item in arr)
+            {
+                string even = "";
+                string odd = "";
+
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        even += item[i].ToString();
+                    }
+                    else
+                    {
+                        odd += item[i].ToString();
+                    }
+                }
+
+                Console.WriteLine(even + " " + odd);
+            }
+            
+            
+        }
+    }
+}

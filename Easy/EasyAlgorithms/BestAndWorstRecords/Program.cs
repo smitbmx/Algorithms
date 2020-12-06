18957ceeb5ef5343301b7d0caf93dc6612dcf481
Ivan Brygar <smitbmx@gmail.com>
2020-12-06 09:59:27 +0800

    Added a task for Best and Worst records.

---
+++ Easy/EasyAlgorithms/BestAndWorstRecords/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BestAndWorstRecords
+{
+    class Program
+    {
+        static int[] breakingRecords(int[] scores)
+        {
+            int[] result = new int[2];
+            int maxRecord = 0;
+            int minRecord = 0;
+            int maxTemp = 0;
+            int minTemp = 0;
+
+            int i = 0;
+            foreach (int score in scores)
+            {
+                if (i == 0)
+                {
+                    maxTemp = score;
+                    minTemp = score;
+                }
+
+                if (score > maxTemp)
+                {
+                    maxTemp = score;
+                    maxRecord++;
+                }
+                else if (score < minTemp)
+                {
+                    minTemp = score;
+                    minRecord++;
+                }
+
+                i++;
+            }
+
+            result[0] = maxRecord;
+            result[1] = minRecord;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] scores = Array.ConvertAll(Console.ReadLine().Split(' '), scoresTemp => Convert.ToInt32(scoresTemp));
+
+            int[] result = breakingRecords(scores);
+
+            Console.WriteLine(string.Join(" ", result));
+
+            Console.ReadKey();
+        }
+    }
+}

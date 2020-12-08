2bc8b359886658bde230d434a326c252ef72bf50
Ivan Brygar <smitbmx@gmail.com>
2020-12-08 10:16:23 +0800

    Added the factorial with recursion implementation.

---
+++ Easy/EasyAlgorithms/FactorialRecursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FactorialRecursion
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+            if (n  == 0)
+            {
+                return 1;
+            }
+
+            return n * factorial(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}

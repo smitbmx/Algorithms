703e1438d30c94aef3142297379de4973530905a
Ivan Brygar <smitbmx@gmail.com>
2020-12-13 19:55:39 +0800

    Added hourglass stub

---
+++ Easy/EasyAlgorithms/Hourglass2DArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hourglass2DArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

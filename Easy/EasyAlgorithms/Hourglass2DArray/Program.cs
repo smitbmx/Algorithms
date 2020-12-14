9845219f9fa73655736c466af2912e5d4519523a
Ivan Brygar <smitbmx@gmail.com>
2020-12-14 17:06:45 +0800

    Solved challenge with 2D array hour glass.

--- Easy/EasyAlgorithms/Hourglass2DArray/Program.cs
+++ Easy/EasyAlgorithms/Hourglass2DArray/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hourglass2DArray
 {
@@ -13,8 +15,20 @@ namespace Hourglass2DArray
                 arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
             }
 
+            List<int> sums = new List<int>();
+            for (int y = 1; y < arr.Length - 1; y++)
+            {
+                for (int x = 1; x < arr.Length - 1; x++)
+                {
+                    int sum = arr[y - 1][x - 1] + arr[y - 1][x] + arr[y - 1][x + 1]
+                                                + arr[y][x]
+                            + arr[y + 1][x - 1] + arr[y + 1][x] + arr[y + 1][x + 1];
 
+                    sums.Add(sum);
+                }
+            }
 
+            Console.WriteLine(sums.Max());
             Console.ReadLine();
         }
     }

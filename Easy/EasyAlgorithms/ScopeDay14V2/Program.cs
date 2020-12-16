33daa034be14a41ff54ee015224bae7e3e354aa9
Ivan Brygar <smitbmx@gmail.com>
2020-12-16 16:44:47 +0800

    Added 2nd version for Scope task. Complexity O(n)

---
+++ Easy/EasyAlgorithms/ScopeDay14V2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace ScopeDay14V2
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+
+        // Add your code here
+        public Difference(int[] numbers)
+        {
+            this.elements = numbers;
+        }
+
+        public void computeDifference()
+        {
+            int min = elements[0];
+            int max = elements[0];
+
+            foreach (int number in elements)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+
+            maximumDifference = max - min;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+        }
+    }
+}

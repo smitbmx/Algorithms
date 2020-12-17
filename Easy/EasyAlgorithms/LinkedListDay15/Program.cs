8734f61aa4c0a44d72f9f6d9d99c4a4b6cfd8ce0
Ivan Brygar <smitbmx@gmail.com>
2020-12-17 20:29:10 +0800

    Solved linked list task recursively.

--- Easy/EasyAlgorithms/LinkedListDay15/Program.cs
+++ Easy/EasyAlgorithms/LinkedListDay15/Program.cs
@@ -18,7 +18,20 @@ namespace LinkedListDay15
     {
         public static Node insert(Node head, int data)
         {
-            //Complete this method
+            if (head == null)
+            {
+                return new Node(data);
+            }
+            else if (head.next == null)
+            {
+                head.next = new Node(data);
+            }
+            else
+            {
+                insert(head.next, data);
+            }
+
+            return head;
         }
 
         public static void display(Node head)

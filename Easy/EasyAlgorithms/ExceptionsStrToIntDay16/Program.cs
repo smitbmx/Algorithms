2d57abf69f75509e188e956de862e3ac8bbfa916
Ivan Brygar <smitbmx@gmail.com>
2020-12-18 18:11:49 +0800

    Task: Exceptions Str to Int

---
+++ Easy/EasyAlgorithms/ExceptionsStrToIntDay16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExceptionsStrToIntDay16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(Convert.ToInt32(S));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bad String");
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}

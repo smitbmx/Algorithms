5b7a72a2535c017024d142eeb12bbcf27a40db30
Ivan Brygar <smitbmx@gmail.com>
2020-12-19 10:18:00 +0800

    Task "MoreExceptions" completed.

---
+++ Easy/EasyAlgorithms/MoreExceptionsDay17/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MoreExceptionsDay17
+{
+    class Calculator
+    {
+        public int power(int n, int p)
+        {
+            if (n < 0 || p < 0)
+            {
+                throw new Exception("n and p should be non-negative");
+            }
+
+            int res = 0;
+
+
+            res = (int)Math.Pow(n, p);
+
+            return res;
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Calculator myCalculator = new Calculator();
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                string[] num = Console.ReadLine().Split();
+                int n = int.Parse(num[0]);
+                int p = int.Parse(num[1]);
+                try
+                {
+                    int ans = myCalculator.power(n, p);
+                    Console.WriteLine(ans);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+
+
+
+        }
+    }
+}

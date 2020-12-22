3d579cbfa2c1e85ad9b750381a8aa0b0ece27530
Ivan Brygar <smitbmx@gmail.com>
2020-12-22 11:42:41 +0800

    Added stub for binary search trees.

---
+++ Easy/EasyAlgorithms/BinarySearchTreesDay22/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BinarySearchTreesDay22
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    class Program
+    {
+        static int getHeight(Node root)
+        {
+            int height = 0;
+
+
+
+            return height;
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
+
+        }
+    }
+}

5ac3716714cbb6c3e89efe4b3598dbacd4cc6b4f
Ivan Brygar <smitbmx@gmail.com>
2020-12-30 21:06:46 +0800

    Code stub preparation

--- Easy/EasyAlgorithms/MoreLinkedListDay24/Program.cs
+++ Easy/EasyAlgorithms/MoreLinkedListDay24/Program.cs
@@ -2,11 +2,67 @@
 
 namespace MoreLinkedListDay24
 {
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        public static Node removeDuplicates(Node head)
         {
-            Console.WriteLine("Hello World!");
+            //Write your code here
+        }
+
+        public static Node insert(Node head, int data)
+        {
+            Node p = new Node(data);
+
+
+            if (head == null)
+                head = p;
+            else if (head.next == null)
+                head.next = p;
+            else
+            {
+                Node start = head;
+                while (start.next != null)
+                    start = start.next;
+                start.next = p;
+
+            }
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
+        static void Main(String[] args)
+        {
+
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            head = removeDuplicates(head);
+            display(head);
         }
     }
 }

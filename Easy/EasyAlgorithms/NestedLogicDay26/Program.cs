d2b44abbb6883636850595578eebf007f7a29f00
Ivan Brygar <smitbmx@gmail.com>
2021-01-01 18:26:27 +0800

    Added stub for the NestedLogic task.

---
+++ Easy/EasyAlgorithms/NestedLogicDay26/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NestedLogicDay26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

dfa46811e45652cb97355f1d72fda35d4deb7bb4
Ivan Brygar <smitbmx@gmail.com>
2021-01-05 18:31:46 +0800

    Added stub for Bitwise AND.

---
+++ Easy/EasyAlgorithms/BitwiseANDDay29/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace BitwiseANDDay29
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+            }
+        }
+    }
+}
